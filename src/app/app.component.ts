0bedfe4075a351ef2eb8dd19e3ac97f9ca6e59c2
EC2 Default User <ec2-user@ip-172-31-35-91.ec2.internal>
2020-01-22 07:42:45 +0000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-angular';
-}
+export class AppComponent implements OnInit, OnDestroy {
+  
+  seconds: number;
+  counterSubscription: Subscription;
+  
+  title = 'ocr-angular';
+  
+  ngOnInit() {
+    const counter= Observable.interval(1000);
+    
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.seconds = value;
+      },
+      (error) => {
+        console.log("error :" + error);
+      },
+      () => {
+        console.log("observable complert")
+      }
+      );
+      }
+      
+      ngOnDestroy() {
+        this.counterSubscription.unsubscribe();
+      }
+  }
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
+import { AuthService } from './services/auth.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './services/user.service';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent,
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    AppareilService,
+    AuthService,
+    UserService,
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
